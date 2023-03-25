768f0cefb313d43458c9d25bb83e7965fc3fb7b3
Husnain <husnainalirafique125@gmail.com>
2023-03-25 05:49:05 +0500

    TextView

---
+++ app/src/main/java/com/example/views/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Actionbar
+        actionBar()
+        //handlingClicks
+        handlingClicks()
+    }
+
+    private fun handlingClicks() {
+        findViewById<Button>(R.id.btToTextViewActivity).setOnClickListener {
+            val intent = Intent(this,TextView::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun actionBar(){
+        supportActionBar?.hide()
+    }
+}
---
+++ app/src/main/java/com/example/views/TextView.kt
@@ -0,0 +1,16 @@
+package com.example.views
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class TextView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_view2)
+    }
+
+
+}
